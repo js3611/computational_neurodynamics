4641059efd5b77f6d8249abded33acaca3547487
Jo Schlemper <js3611@doc-js3611.lib.ic.ac.uk>
2015-11-16 15:59:25 +0000

    working on q(b)

--- IzModularNetwork.py
+++ IzModularNetwork.py
@@ -9,98 +9,133 @@ import matplotlib.pyplot as plt
 # create a modular network using IzNetwork with
 # 8 modules of 100 exhibitory neurons and 1 module of 200 inhibitory neurons
 
-F_e2e = 17
-F_e2i = 50
-F_i2e = 2
-F_i2i = 1
-D = 1
-Dmax = 20
-n_ex_layer = 8
+def create_izModularNetwork(p=0):
+    # p = rewiring probability between exhibitory to exhibitory connections
 
-N_ex = 100
-N_in = 200
-idx_in = 8
+    F_e2e = 17
+    F_e2i = 50
+    F_i2e = 2
+    F_i2i = 1
+    D = 1
+    Dmax = 20
+    n_ex_layer = 8
 
-population = [N_ex] * n_ex_layer + [N_in]
-net = IzNetwork(population, Dmax)
+    N_ex = 100
+    N_in = 200
+    idx_in = 8
 
-# make empty connectivity matrix between each layer
-for i in xrange(len(population)):
-    for j in xrange(len(population)):
-        net.layer[i].S[j] = np.zeros([population[i], population[j]])
+    population = [N_ex] * n_ex_layer + [N_in]
+    net = IzNetwork(population, Dmax)
 
-for i in xrange(n_ex_layer):
-    # make each module (layer) using random connections
-    r = rand.random(N_ex)
-    net.layer[i].N = N_ex
-    net.layer[i].a = 0.02 * np.ones(N_ex)
-    net.layer[i].b = 0.20 * np.ones(N_ex)
-    net.layer[i].c = -65 + 15*(r**2)
-    net.layer[i].d = 8 - 6*(r**2)
-
-    # connectivity
-    S = np.zeros([N_ex, N_ex])
-    n_connection = 0
-    while n_connection < 1000:
-        src = rand.random_integers(0, N_ex-1)
-        dst = rand.random_integers(0, N_ex-1)
-        if src != dst and S[dst, src] != 1:
-            S[dst, src] = 1
-            n_connection += 1
-
-    net.layer[i].S[i] = S
-    net.layer[i].factor[i] = F_e2e
-    d = 20 * rand.random(N_ex*N_ex).reshape((N_ex, N_ex))
-    net.layer[i].delay[i] = d
-
-# rewire exhibitory neurons
-
-# inhibitory neuron setting
-
-r = rand.random(N_in)
-net.layer[idx_in].N = N_in
-net.layer[idx_in].a = 0.02 * np.ones(N_in)
-net.layer[idx_in].b = 0.25 * np.ones(N_in)
-net.layer[idx_in].c = -65 + 15*(r**2)
-net.layer[idx_in].d = 2 - 6*(r**2)
-
-# 1. connection from inhibitory to exhibitory with random weight
-for i in xrange(n_ex_layer):
-    net.layer[i].S[idx_in] = -1 * rand.random(N_ex * N_in).reshape((N_ex, N_in))
-    net.layer[i].delay[idx_in] = D * np.ones([N_ex, N_in])
-    net.layer[i].factor[idx_in] = F_i2e
-
-# 2. connections from exhibitory to inhibitory
-# each inhibitory neuron has focal connection from 4 exhibitory neuron from the same module
-for i in xrange(N_in):
-    # pick a random module
-    m = rand.random_integers(0, n_ex_layer)
-    # pick 4 random neurons
-    rand_neurons = rand.random_integers(0, N_ex-1, 4)
-    S = net.layer[idx_in].S[m]
-    S[i][rand_neurons] = rand.random(4)
-
-for i in xrange(n_ex_layer):
-    net.layer[idx_in].factor[i] = F_e2i
-    net.layer[idx_in].delay[i] = D
-
-# 3. connections from inhibitory to inhibitory
-net.layer[idx_in].S[idx_in] = -1 * rand.random(N_in * N_in).reshape((N_in, N_in))
-net.layer[idx_in].factor[idx_in] = F_i2i
-net.layer[idx_in].delay[idx_in] = D
-
-# Print out the matrix for fun
-vertical = {}
-for i in xrange(len(population)):
-    vertical[i] = np.concatenate(net.layer[i].S.values(), axis=1)
-network_matrix = np.concatenate(vertical.values(), axis=0)
-plt.matshow(network_matrix, fignum=100, cmap=plt.cm.gray)
-plt.show()
+    # make empty connectivity matrix between each layer
+    for i in xrange(len(population)):
+        for j in xrange(len(population)):
+            net.layer[i].S[j] = np.zeros([population[i], population[j]])
+            if i != idx_in and j != idx_in:
+                net.layer[i].factor[j] = F_e2e
+                d = 20 * rand.random(N_ex*N_ex).reshape((N_ex, N_ex))
+                net.layer[i].delay[j] = d
 
 
-# connect each module again randomly
+    # exhibitory module
+    for i in xrange(n_ex_layer):
+        # make each module (layer) using random connections
+        r = rand.random(N_ex)
+        net.layer[i].N = N_ex
+        net.layer[i].a = 0.02 * np.ones(N_ex)
+        net.layer[i].b = 0.20 * np.ones(N_ex)
+        net.layer[i].c = -65 + 15*(r**2)
+        net.layer[i].d = 8 - 6*(r**2)
+
+        # connectivity
+        S = np.zeros([N_ex, N_ex])
+        n_connection = 0
+        while n_connection < 1000:
+            src = rand.random_integers(0, N_ex-1)
+            dst = rand.random_integers(0, N_ex-1)
+            if src != dst and S[dst, src] != 1:
+                S[dst, src] = 1
+                n_connection += 1
+
+        net.layer[i].S[i] = S
+
+
+    # inhibitory neuron setting
+
+    r = rand.random(N_in)
+    net.layer[idx_in].N = N_in
+    net.layer[idx_in].a = 0.02 * np.ones(N_in)
+    net.layer[idx_in].b = 0.25 * np.ones(N_in)
+    net.layer[idx_in].c = -65 + 15*(r**2)
+    net.layer[idx_in].d = 2 - 6*(r**2)
+
+    # 1. connection from inhibitory to exhibitory with random weight
+    for i in xrange(n_ex_layer):
+        net.layer[i].S[idx_in] = -1 * rand.random(N_ex * N_in).reshape((N_ex, N_in))
+        net.layer[i].delay[idx_in] = D * np.ones([N_ex, N_in])
+        net.layer[i].factor[idx_in] = F_i2e
+
+    # 2. connections from exhibitory to inhibitory
+    # each inhibitory neuron has focal connection from 4 exhibitory neuron from the same module
+    for i in xrange(N_in):
+        # pick a random module
+        m = rand.random_integers(0, n_ex_layer)
+        # pick 4 random neurons
+        rand_neurons = rand.random_integers(0, N_ex-1, 4)
+        S = net.layer[idx_in].S[m]
+        S[i][rand_neurons] = rand.random(4)
+
+    for i in xrange(n_ex_layer):
+        net.layer[idx_in].factor[i] = F_e2i
+        net.layer[idx_in].delay[i] = D * np.ones([N_in, N_ex])
+
+    # 3. connections from inhibitory to inhibitory
+    net.layer[idx_in].S[idx_in] = -1 * rand.random(N_in * N_in).reshape((N_in, N_in))
+    net.layer[idx_in].factor[idx_in] = F_i2i
+    net.layer[idx_in].delay[idx_in] = D * np.ones([N_in, N_in])
+
+
+
+    # (Rewiring) connect each module again randomly
+    # for each node, with some probability, rewire!
+
+    for l in xrange(n_ex_layer):
+        for node in xrange(N_ex):
+            if rand.random() < p:
+                # pick random edge
+                S = net.layer[l].S[l]
+                edge_indices = [i for i in xrange(N_ex) if S[i, node]]
+                if len(edge_indices) > 0:
+                    random_index = rand.random_integers(0, len(edge_indices)-1)
+                    target_index = edge_indices[random_index]
+                    # remove the edge
+                    S[target_index, node] = 0
+                    # pick a new node in a new module
+                    rand_module = rand.random_integers(0, n_ex_layer-1)
+                    if rand_module >= l:
+                        rand_module = (rand_module + 1) % n_ex_layer
+
+                    # rewire
+                    rand_target = rand.random_integers(0, N_ex-1)
+                    target_S = net.layer[rand_module].S[l]
+                    target_S[rand_target, node] = 1
+
+    return net
+
+
+
+
+
+
+
+
+
+
+
 
 # 1. consider a random module and pick an edge from n1->n2
+
+
 # 2. pick another random module & a random neuron n'
 # 3. rewire (ie. delete the old edge and create a new edge from n1->n'
 
--- Q1.py
+++ Q1.py
@@ -1,1 +1,70 @@
 __author__ = 'js3611'
+
+from IzModularNetwork import create_izModularNetwork
+import numpy as np
+import numpy.random as rand
+import matplotlib.pyplot as plt
+
+
+
+
+
+############# Q1 (a) #############
+# p_range = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+p_range = [0]
+
+for p in p_range:
+    net = create_izModularNetwork(p)
+
+
+    # Print out the matrix for fun
+    vertical = {}
+    for i in xrange(len(net.layer)):
+        vertical[i] = np.concatenate(net.layer[i].S.values(), axis=1)
+    network_matrix = np.concatenate(vertical.values(), axis=0)
+    plt.matshow(network_matrix, fignum=100, cmap=plt.cm.gray)
+    plt.show()
+
+
+############# Q1(b) #############
+
+
+T  = 1000  # Simulation time
+Ib = 15.0    # extra current to be injected
+
+net = create_izModularNetwork(0.1)
+
+## Initialise layers
+for lr in xrange(len(net.layer)):
+  net.layer[lr].v = -65 * np.ones(net.layer[lr].N)
+  net.layer[lr].u = net.layer[lr].b * net.layer[lr].v
+  net.layer[lr].firings = np.array([])
+
+
+## SIMULATE
+for t in xrange(T):
+
+   # Deliver a constant base current to layer 1
+
+   for i in xrange(len(net.layer)):
+       net.layer[i].I = Ib * rand.poisson(0.01, net.layer[i].N)
+
+   net.Update(t)
+
+
+
+## Retrieve firings and add Dirac pulses for presentation
+firings = np.concatenate([net.layer[i].firings for i in xrange(net.layer)], axis=0)
+
+
+## Raster plots of firings
+if firings.size != 0:
+  plt.scatter(firings[:, 0], firings[:, 1] + 1, marker='.')
+  plt.xlim(0, T)
+  plt.ylabel('Neuron number')
+  plt.ylim(0, 1000+1)
+  plt.title('Population 1 firings')
+
+
+
+plt.show()
